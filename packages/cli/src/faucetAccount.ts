31d42e6439a9ea6bae9da4d7a05671ba66c500d7
Sg <shogo1104@gmail.com>
2020-04-27 12:06:46 +0900

    lint fix

---
+++ packages/cli/src/faucetAccount.ts
@@ -0,0 +1,24 @@
+export default {
+  mnemonic: [
+    'orbit',
+    'avoid',
+    'identify',
+    'shoot',
+    'volcano',
+    'erupt',
+    'chuckle',
+    'tell',
+    'devote',
+    'ensure',
+    'tenant',
+    'couple',
+    'indoor',
+    'endorse',
+    'zebra'
+  ],
+  secret: 'f014be0bd04d6ef12c11e7c65c6cab7a4d571beb',
+  amount: '46436636122',
+  pkh: 'tz1itBLXvKVyUxjB1bXVC5uNBQXLqZotdoeC',
+  password: 'lLDv8wNiZK',
+  email: 'adoijlyg.amhbjvrd@tezos.example.org'
+}
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,21 +1,40 @@
-import {TezosMessageUtils, TezosWalletUtil, StoreType, TezosNodeWriter} from 'conseiljs'
-import {config} from 'dotenv'
+import {
+  TezosMessageUtils,
+  TezosWalletUtil,
+  StoreType,
+  TezosNodeWriter
+} from 'conseiljs'
+import { config } from 'dotenv'
 config()
-import LightClient, {StateManager, SyncManager, CheckpointManager} from '@cryptoeconomicslab/plasma-light-client'
-import {TzWallet} from '@cryptoeconomicslab/tezos-wallet'
-import {Balance} from '@cryptoeconomicslab/wallet'
-import {Address, Bytes} from '@cryptoeconomicslab/primitives'
+import LightClient, {
+  StateManager,
+  SyncManager,
+  CheckpointManager
+} from '@cryptoeconomicslab/plasma-light-client'
+import { TzWallet } from '@cryptoeconomicslab/tezos-wallet'
+import { Balance } from '@cryptoeconomicslab/wallet'
+import { Address, Bytes } from '@cryptoeconomicslab/primitives'
 import leveldown from 'leveldown'
-import {LevelKeyValueStore} from '@cryptoeconomicslab/level-kvs'
-import {AdjudicationContract, DepositContract, CommitmentContract, ERC20Contract, OwnershipPayoutContract} from '@cryptoeconomicslab/tezos-contract'
+import { LevelKeyValueStore } from '@cryptoeconomicslab/level-kvs'
+import {
+  AdjudicationContract,
+  DepositContract,
+  CommitmentContract,
+  ERC20Contract,
+  OwnershipPayoutContract
+} from '@cryptoeconomicslab/tezos-contract'
 import Cli from 'cac'
 const cli = Cli()
-import {TzCoder} from '@cryptoeconomicslab/tezos-coder'
-import {setupContext} from '@cryptoeconomicslab/context'
-setupContext({coder: TzCoder})
+import { TzCoder } from '@cryptoeconomicslab/tezos-coder'
+import { setupContext } from '@cryptoeconomicslab/context'
+import faucetAccount from './faucetAccount'
+setupContext({ coder: TzCoder })
 
 async function instantiate() {
-  const kvs = new LevelKeyValueStore(Bytes.fromString('tezos-liteclient-cli'), leveldown('.db'))
+  const kvs = new LevelKeyValueStore(
+    Bytes.fromString('tezos-liteclient-cli'),
+    leveldown('.db')
+  )
   const eventDb = await kvs.bucket(Bytes.fromString('event'))
 
   // TODO: fix light client interface
@@ -23,15 +42,22 @@ async function instantiate() {
   const apiKey = process.env.TEZOS_APIKEY || 'hooman'
   const url = process.env.CONCEIL_ENDPOINT
   const tezosNodeEndpoint = process.env.TEZOS_NODE_ENDPOINT
-  const adjudicationContractAddress = TezosMessageUtils.writeAddress(process.env.ADJUDICATION_CONTRACT_ADDRESS as string)
-  const commitmentContractAddress = TezosMessageUtils.writeAddress(process.env.COMMITMENT_CONTRACT_ADDRESS as string)
+  const adjudicationContractAddress = TezosMessageUtils.writeAddress(process.env
+    .ADJUDICATION_CONTRACT_ADDRESS as string)
+  const commitmentContractAddress = TezosMessageUtils.writeAddress(process.env
+    .COMMITMENT_CONTRACT_ADDRESS as string)
   if (!url) {
     throw new Error('must require CONCEIL_ENDPOINT')
   }
   if (!tezosNodeEndpoint) {
     throw new Error('must require MAIN_CHAIN_HOST')
   }
-  const wallet = new TzWallet(await TezosWalletUtil.restoreIdentityWithSecretKey(process.env.PRIVATE_KEY as string), tezosNodeEndpoint, {url, apiKey, network})
+  const wallet = new TzWallet(
+    await TezosWalletUtil.restoreIdentityWithSecretKey(process.env
+      .PRIVATE_KEY as string),
+    tezosNodeEndpoint,
+    { url, apiKey, network }
+  )
 
   function depositContractFactory(address) {
     return new DepositContract(address, eventDb, wallet)
@@ -46,22 +72,52 @@ async function instantiate() {
   const checkpointDb = await kvs.bucket(Bytes.fromString('checkpoint'))
   const checkpointManager = new CheckpointManager(checkpointDb)
 
-  const adjudicationContract = new AdjudicationContract(Address.from(adjudicationContractAddress), eventDb, wallet)
-  const commitmentContract = new CommitmentContract(Address.from(commitmentContractAddress), eventDb, wallet)
-  const erc20Contract = new ERC20Contract(Address.from(commitmentContractAddress), eventDb, wallet)
-  const ownershipPayoutContract = new OwnershipPayoutContract(Address.from(commitmentContractAddress), eventDb, wallet)
-  function tokenContractFactory(address : Address) {
+  const adjudicationContract = new AdjudicationContract(
+    Address.from(adjudicationContractAddress),
+    eventDb,
+    wallet
+  )
+  const commitmentContract = new CommitmentContract(
+    Address.from(commitmentContractAddress),
+    eventDb,
+    wallet
+  )
+  const erc20Contract = new ERC20Contract(
+    Address.from(commitmentContractAddress),
+    eventDb,
+    wallet
+  )
+  const ownershipPayoutContract = new OwnershipPayoutContract(
+    Address.from(commitmentContractAddress),
+    eventDb,
+    wallet
+  )
+  function tokenContractFactory(address: Address) {
     return new ERC20Contract(address, eventDb, wallet)
   }
 
   const mainChainEnv = process.env.MAIN_CHAIN_ENV || 'local'
-  const config = await import (`../config.${mainChainEnv}`)
+  const config = await import(`../config.${mainChainEnv}`)
 
-  return new LightClient(wallet, kvs, adjudicationContract, depositContractFactory, tokenContractFactory, commitmentContract, ownershipPayoutContract, stateManager, syncManager, checkpointManager, config)
+  return new LightClient(
+    wallet,
+    kvs,
+    adjudicationContract,
+    depositContractFactory,
+    tokenContractFactory,
+    commitmentContract,
+    ownershipPayoutContract,
+    stateManager,
+    syncManager,
+    checkpointManager,
+    config
+  )
 }
 
-const depositContractAddress = TezosMessageUtils.writeAddress(process.env.DEPOSIT_CONTRACT_ADDRESS as string)
-const tokenAddress = TezosMessageUtils.writeAddress(process.env.TOKEN_ADDRESS as string)
+const depositContractAddress = TezosMessageUtils.writeAddress(process.env
+  .DEPOSIT_CONTRACT_ADDRESS as string)
+const tokenAddress = TezosMessageUtils.writeAddress(process.env
+  .TOKEN_ADDRESS as string)
 
 export default async function initialize() {
   const lightClient = await instantiate()
@@ -79,68 +135,99 @@ console.log(
 */
 // console.log(TezosMessageUtils.writeAddress('KT1LBdoNuEUQyBjN85rQzHqJFXjWZqmN8HeP'))
 
-cli
-  .command('deposit <amount>', 'Deposit')
-  .action(async(amount, options) => {
-    const lightClient = await initialize()
-    console.log('deposit', amount)
-    await lightClient.deposit(Number(amount), tokenAddress
+cli.command('deposit <amount>', 'Deposit').action(async (amount, options) => {
+  const lightClient = await initialize()
+  console.log('deposit', amount)
+  await lightClient.deposit(
+    Number(amount),
+    tokenAddress
     // TezosMessageUtils.writeAddress('KT1UxjVKVMsKRkwvG9XPqXBRNP8t3rqnmq3J')
-    )
-    console.log('deposited')
-    process.exit();
-  })
-cli
-  .command('balance', 'getBalance')
-  .action(async options => {
-    const lightClient = await initialize()
-    console.log('getBalance start')
-    try {
-      const balances = await lightClient.getBalance()
-      console.log('Balance L2:', balances[0].amount / 1000000, 'tz')
-    } catch (e) {
-      console.error(`getBalance (L2) failed - ${e.message}`)
-    }
-    try {
-      const l1balance : Balance = await lightClient['wallet'].getL1Balance()
-      console.log('Balance L1:', Number(l1balance.value.raw) / 1000000, 'tz')
-    } catch (e) {
-      console.error(`getL1Balance failed - ${e.message}`, e)
-    }
-    console.log('getBalance end');
-    process.exit();
-  })
+  )
+  console.log('deposited')
+  process.exit()
+})
+cli.command('balance', 'getBalance').action(async options => {
+  const lightClient = await initialize()
+  console.log('getBalance start')
+  try {
+    const balances = await lightClient.getBalance()
+    console.log('Balance L2:', balances[0].amount / 1000000, 'tz')
+  } catch (e) {
+    console.error(`getBalance (L2) failed - ${e.message}`)
+  }
+  try {
+    const l1balance: Balance = await lightClient['wallet'].getL1Balance()
+    console.log('Balance L1:', Number(l1balance.value.raw) / 1000000, 'tz')
+  } catch (e) {
+    console.error(`getL1Balance failed - ${e.message}`, e)
+  }
+  console.log('getBalance end')
+  process.exit()
+})
 cli
   .command('transfer <amount> <to>', 'transfer')
-  .action(async(amount, to, options) => {
+  .action(async (amount, to, options) => {
     const lightClient = await initialize()
-    await lightClient.transfer(amount, tokenAddress, '0x' + TezosMessageUtils.writeAddress(to))
-    process.exit();
+    await lightClient.transfer(
+      amount,
+      tokenAddress,
+      '0x' + TezosMessageUtils.writeAddress(to)
+    )
+    process.exit()
   })
 cli
   .command('unlock <mnemonic> <email> <password> <address>', 'Import')
-  .action(async(mnemonic, email, password, address, options) => {
-    const a = await TezosWalletUtil.unlockFundraiserIdentity(mnemonic, email, password, address)
+  .action(async (mnemonic, email, password, address, options) => {
+    const a = await TezosWalletUtil.unlockFundraiserIdentity(
+      mnemonic,
+      email,
+      password,
+      address
+    )
     console.log(a.privateKey, a.publicKeyHash)
-    process.exit();
+    process.exit()
   })
 
-cli
-.command('activate', 'Activate')
-.action(async()=>{
+cli.command('activate', 'Activate').action(async () => {
   const tezosNodeEndpoint = process.env.TEZOS_NODE_ENDPOINT as string
-  const faucetAccount = require('../account.json')
-  const faucetKeys = await TezosWalletUtil.unlockFundraiserIdentity(faucetAccount.mnemonic.join(' '), faucetAccount.email, faucetAccount.password, faucetAccount.pkh);
+  const faucetKeys = await TezosWalletUtil.unlockFundraiserIdentity(
+    faucetAccount.mnemonic.join(' '),
+    faucetAccount.email,
+    faucetAccount.password,
+    faucetAccount.pkh
+  )
   console.log(faucetAccount)
   console.log(faucetKeys)
-  const nodeResult1 = await TezosNodeWriter.sendIdentityActivationOperation(tezosNodeEndpoint, faucetKeys, faucetAccount.secret);
-  if (JSON.parse(nodeResult1["operationGroupID"])[0].id === 'failure') throw new Error(`sendIdentityActivationOperation: ${JSON.parse(nodeResult1["operationGroupID"])[0].msg}`)
-  const keys = await TezosWalletUtil.getKeysFromMnemonicAndPassphrase(faucetAccount.mnemonic.join(' '), faucetAccount.password, StoreType.Mnemonic)
-  const nodeResult2 = await TezosNodeWriter.sendKeyRevealOperation(tezosNodeEndpoint, keys, 50000);
-  if (JSON.parse(nodeResult2["operationGroupID"])[0].id === 'failure') throw new Error(`sendKeyRevealOperation: ${JSON.parse(nodeResult2["operationGroupID"])[0].msg}`)
+  const nodeResult1 = await TezosNodeWriter.sendIdentityActivationOperation(
+    tezosNodeEndpoint,
+    faucetKeys,
+    faucetAccount.secret
+  )
+  if (JSON.parse(nodeResult1['operationGroupID'])[0].id === 'failure')
+    throw new Error(
+      `sendIdentityActivationOperation: ${
+        JSON.parse(nodeResult1['operationGroupID'])[0].msg
+      }`
+    )
+  const keys = await TezosWalletUtil.getKeysFromMnemonicAndPassphrase(
+    faucetAccount.mnemonic.join(' '),
+    faucetAccount.password,
+    StoreType.Mnemonic
+  )
+  const nodeResult2 = await TezosNodeWriter.sendKeyRevealOperation(
+    tezosNodeEndpoint,
+    keys,
+    50000
+  )
+  if (JSON.parse(nodeResult2['operationGroupID'])[0].id === 'failure')
+    throw new Error(
+      `sendKeyRevealOperation: ${
+        JSON.parse(nodeResult2['operationGroupID'])[0].msg
+      }`
+    )
   console.log(nodeResult1)
   console.log(nodeResult2)
-  process.exit();
+  process.exit()
 })
 cli.help()
 cli.parse()
